a0d492a4b06b28fdc82dd896e9176b4c645c5284
AsterOvO <lzy1048632280@gmail.com>
2026-09-04 10:55:26 +0800

    feat: add node and go docker services

--- config.example.js
+++
@@ -1,5 +0,0 @@
-// 复制为 config.local.js 后填入自己的 Apex Legends Status API Key。
-window.APP_CONFIG = Object.freeze({
-  apexApiKey: "",
-  apexApiUrl: "https://api.apexlegendsstatus.com/maprotation?version=2"
-});
